fde5de235a5718952be1ff35a993e7122fcb3ef4
Pansit Wattanaprasobsuk <pansit.wattanaprasobsuk@thomsonreuters.com>
2017-06-29 16:28:47 +0700

    page 13

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    var helloWorld = 'Welcome to React'
+    return (
+      <div className="App">
+        <h2>{helloWorld}</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
